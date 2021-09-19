6f73a2a4c2d04b522eb6ab683576949f484a0d5c
suryudu <65017588+suryudu@users.noreply.github.com>
2021-09-19 18:22:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {Route,BrowserRouter as Router,Switch} from "react-router-dom";
 import Match from "./components/Teams/Match";
 import MyPlayers from './components/playing11/MyPlayers';
 import Generate from "./components/playing11/Generate";
+import AdminRoute from './components/AdminRoute';
+import UpdatePoints from './components/points/UpdatePoints';
+import UpdatedTeams from './components/points/UpdatedTeams';
 
 function App() {
   return (
@@ -21,7 +24,15 @@ function App() {
         <Route path="/generate/:id" >
           <Generate />
         </Route>
-         
+        <Route path="/adminroute/:id/:team1/:team2">
+          <AdminRoute />
+        </Route>
+        <Route path="/updatepoints/:id">
+          <UpdatePoints />
+        </Route>
+         <Route path="/updatedteams/:id">
+          <UpdatedTeams />
+         </Route>
       </Switch>
     </Router>
   );
